1e5b904e8c0b27d1709b9bbeb56011b2175d6b30
Lucas Galdino <lucasgaldino7@gmail.com>
2021-07-08 11:06:09 -0300

    portas parte 2

---
+++ portas/components/Porta.tsx
@@ -0,0 +1,26 @@
+import styles from "../styles/Porta.module.css"
+import PortaModel from '../model/porta'
+
+interface PortaProps {
+    value: PortaModel
+    onChange: (novaPorta: PortaModel) => void
+}
+
+export default function Porta(props: PortaProps) {
+    const porta = props.value
+    const selecionada = porta.selecionada ? styles.selecionada : ''
+
+    const alternarSelecao = e => props.onChange(porta.alternarSelecao())
+
+    return (
+        <div className={styles.area} onClick={alternarSelecao}>
+            <div className={`${styles.estrutura} ${selecionada}`}>
+                <div className={styles.porta}>
+                    <div className={styles.numero}>{porta.numero}</div>
+                    <div className={styles.macaneta}></div>
+                </div>
+            </div>
+            <div className={styles.chao}></div>
+        </div>
+    )
+}
--- portas/pages/index.jsx
+++
@@ -1,9 +0,0 @@
-import Presente from "../components/Presente";
-
-export default function Home() {
-  return (
-    <div>
-      <Presente />
-    </div>
-  )
-}
