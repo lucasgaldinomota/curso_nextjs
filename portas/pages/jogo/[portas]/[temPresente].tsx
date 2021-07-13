46ffb7f6dbb0318d76a0bec15b489b297dc611b5
Lucas Galdino <lucasgaldino7@gmail.com>
2021-07-13 10:03:25 -0300

    finalizando projeto portas

--- portas/pages/jogo/[portas]/[temPresente].tsx
+++ portas/pages/jogo/[portas]/[temPresente].tsx
@@ -5,10 +5,21 @@ import { atualizarPortas, criarPortas } from "../../../functions/portas"
 import Link from "next/link"
 import { useRouter } from "next/router"
 
-export default function jogo() {
+export default function Jogo() {
     const router = useRouter()
 
+    const [valido, setValido] = useState(false)
     const [portas, setPortas] = useState([])
+
+    useEffect(() => {
+        const portas = +router.query.portas
+        const temPresente = +router.query.temPresente
+
+        const qtdePortasValida = portas >= 3 && portas <= 100
+        const temPresenteValido = temPresente >= 1 && temPresente <= portas
+
+        setValido(qtdePortasValida && temPresenteValido)
+    }, [portas, router.query.portas, router.query.temPresente])
 
     useEffect(() => {
         const portas = +router.query.portas
@@ -26,10 +37,13 @@ export default function jogo() {
     return (
         <div id={styles.jogo}>
             <div className={styles.portas}>
-                {renderizarPortas()}
+                {valido ?
+                    renderizarPortas() :
+                    <h1>Valores inválidos</h1>
+                }
             </div>
             <div className={styles.botoes}>
-                <Link href="/">
+                <Link href="/" passHref>
                     <button>Reiniciar Jogo</button>
                 </Link>
             </div>
