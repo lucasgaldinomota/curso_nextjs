239c83136328b6c8fe0a28ec8d9aea67b31bb4fa
Lucas Galdino <lucasgaldino7@gmail.com>
2021-07-21 19:08:21 -0300

    iniciando projeto com tailwind

---
+++ tarefas/src/pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <div className={`
+        flex flex-col justify-center items-center h-screen
+        text-white bg-gradient-to-tr from-purple-500 to-yellow-600
+        `}>
+            <span>1</span>
+            <span>2</span>
+            <span>3</span>
+        </div>
+    )
+}
