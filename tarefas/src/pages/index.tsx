d819ee76df40226437a44f20926b2afa214a6ecf
Lucas Galdino <lucasgaldino7@gmail.com>
2021-07-22 11:11:28 -0300

    projeto tarefas 22/07

--- tarefas/src/pages/index.tsx
+++ tarefas/src/pages/index.tsx
@@ -1,12 +1,39 @@
+import Tarefa from "../model/Tarefa"
+import tarefasIniciais from '../data/mock'
+
 export default function Home() {
+
+    let tarefas = tarefasIniciais
+    tarefas = tarefas.filtrarAtivas()
+    tarefas = tarefas.filtrarConcluidas()
+    //tarefas = tarefas.removerFiltro()
+    tarefas = tarefas.excluirConcluidas()
+
+    tarefas = tarefas.adicionarTarefa(Tarefa.criarConcluida(6, 'Lavar os pratos!!!'))
+    tarefas = tarefas.adicionarTarefa(Tarefa.criarAtiva(7, 'Cuidar das crianças!'))
+    tarefas = tarefas.excluirConcluidas()
+
+    tarefas = tarefas.modificarTarefa(tarefas.itens[2].alternarStatus())
+    tarefas = tarefas.modificarTarefa(tarefas.itens[1].alternarStatus())
+
+    function renderizarTarefas() {
+        return tarefas.itens.map(tarefa => {
+            return (
+                <div key={tarefa.id}>
+                    <span>{tarefa.id} | </span>
+                    <span>{tarefa.descricao} | </span>
+                    <span>{tarefa.concluida ? 'Concluída' : 'Ativa'}</span>
+                </div>
+            )
+        })
+    }
+
     return (
         <div className={`
         flex flex-col justify-center items-center h-screen
         text-white bg-gradient-to-tr from-purple-500 to-yellow-600
         `}>
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
+            {renderizarTarefas()}
         </div>
     )
 }
