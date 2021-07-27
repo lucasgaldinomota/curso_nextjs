3775f66010f144e9d4018de130701047f7082cee
Lucas Galdino <lucasgaldino7@gmail.com>
2021-07-27 14:32:23 -0300

    projeto template 27/07 pt2

--- admin-template/src/pages/autenticacao.tsx
+++ admin-template/src/pages/autenticacao.tsx
@@ -1,10 +1,17 @@
 import { useState } from "react"
 import AuthInput from "../components/auth/AuthInput"
+import { IconeAtencao } from "../components/icons"
 
 export default function Autenticacao() {
+    const [erro, setErro] = useState(null)
     const [modo, setModo] = useState<'login' | 'cadastro'>('login')
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
+
+    function exibirErro(msg, tempoEmSegundos = 5) {
+        setErro(msg)
+        setTimeout(() => setErro(null), tempoEmSegundos * 1000)
+    }
 
     function submeter() {
         if (modo === 'login') {
@@ -26,6 +33,19 @@ export default function Autenticacao() {
                 <h1 className={`text-3xl font-bold mb-5`}>
                     {modo === 'login' ? 'Entre com sua conta' : 'Cadastre-se na Plataforma'}
                 </h1>
+
+                {erro ? (
+                    <div className={`
+                                    flex items-center
+                                    bg-red-400 text-white py-3 px-5 my-2
+                                    border border-red-700 rounded-lg
+                                `}>
+                        {IconeAtencao()}
+                        <span className="ml-3">{erro}</span>
+                    </div>
+                ) : false}
+
+
                 <AuthInput
                     label="Email"
                     tipo="email"
