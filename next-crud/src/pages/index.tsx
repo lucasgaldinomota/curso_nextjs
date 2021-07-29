38d074b7a57c18f85b562aa64c3e91d99e2a76a6
Lucas Galdino <lucasgaldino7@gmail.com>
2021-07-29 12:32:34 -0300

    iniciando projeto crud

---
+++ next-crud/src/pages/index.tsx
@@ -0,0 +1,36 @@
+import Layout from '../components/Layout'
+import Tabela from '../components/Tabela'
+import Cliente from '../core/Cliente'
+
+export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '4'),
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Excluir... ${cliente.nome}`)
+  }
+
+  return (
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+      <Layout titulo="Cadastro Simples">
+        <Tabela clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        />
+      </Layout>
+    </div>
+  )
+}
